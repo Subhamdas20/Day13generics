98fcd337707f550a30e45143bf2a0e2acc0d4ba6
Subhamdas20 <subham21.sd@gmail.com>
2021-10-02 19:19:57 +0530

    found the maximum from three intergers and passed three given test cases

--- src/main/java/com/bz/findmax/FindMax.java
+++ src/main/java/com/bz/findmax/FindMax.java
@@ -1,4 +1,15 @@
 package com.bz.findmax;
-
+import java.lang.*;
 public class FindMax {
+
+    public int findMaximun(Integer num1 , Integer num2 , Integer num3){
+        int max = num1;
+        if(num2.compareTo(max)>0){
+            max=num2;
+        }
+        if(num3.compareTo(max)>0){
+            max=num3;
+        }
+        return max;
+    }
 }
