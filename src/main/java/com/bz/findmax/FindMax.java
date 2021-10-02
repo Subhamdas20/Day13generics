97d8a8e703cde8d1dee4e7533a6e3605e4869070
Subhamdas20 <subham21.sd@gmail.com>
2021-10-02 22:19:11 +0530

    extended the printMax method to also print the max and call it internally from testMaximum method

--- src/main/java/com/bz/findmax/FindMax.java
+++ src/main/java/com/bz/findmax/FindMax.java
@@ -8,9 +8,9 @@ public class FindMax <T extends Comparable<T>> {
         this.z = z;
     }
     public T testMaximum(){
-      return   FindMax.findMaximum(x , y , z);
+      return   FindMax.testMaximum(x , y , z);
     }
-    public static  <T extends Comparable<T>> T findMaximum(T x , T y , T z){
+    public static  <T extends Comparable<T>> T testMaximum(T x , T y , T z){
         T max =  x;
         if(y.compareTo(max)>0){
             max=y;
@@ -18,9 +18,13 @@ public class FindMax <T extends Comparable<T>> {
         if(z.compareTo(max)>0){
             max=z;
         }
+        printMax(x,y,z,max);
         return max;
     }
     public static <T>void printMax(T ... a){
-
+        for(T i:a) {
+            System.out.print(i+" ");
+        }
+        System.out.println();
     }
 }
