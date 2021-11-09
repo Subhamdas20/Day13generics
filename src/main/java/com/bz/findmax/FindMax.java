98077828084d3e02e2c852681f3610fd3af9ec58
Subhamdas20 <subham21.sd@gmail.com>
2021-11-09 22:33:59 +0530

    Added : uc5 print max

--- src/main/java/com/bz/findmax/FindMax.java
+++ src/main/java/com/bz/findmax/FindMax.java
@@ -1,6 +1,10 @@
 package com.bz.findmax;
 import java.lang.*;
 public class FindMax <T extends Comparable<T>> {
+    /**
+     * FindMax is a constructor
+     * @param x , y ,z are three input from user
+     * */
     T x , y , z ;
     public FindMax(T x, T y, T z) {
         this.x = x;
@@ -10,6 +14,14 @@ public class FindMax <T extends Comparable<T>> {
     public T testMaximum(){
       return   FindMax.testMaximum(x , y , z);
     }
+
+    /**
+     * testMaximum is used to check maximum of 3 inputs
+     * @param x is user input
+     * @param y is user input
+     * @param z is user input
+     * */
+
     public static  <T extends Comparable<T>> T testMaximum(T x , T y , T z){
         T max =  x;
         if(y.compareTo(max)>0){
